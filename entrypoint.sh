b80df7c1302a28a68a760398072d16d81edb4b7a
suken27 <suken2727@gmail.com>
2026-04-29 01:20:42 +0200

    fix running issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,11 @@
 # Set up cron job if CRON_EXPRESSION is set
 if [ -n "$CRON_EXPRESSION" ]; then
     echo "Setting up cron job with expression: $CRON_EXPRESSION"
-    echo "$CRON_EXPRESSION root python /app/script.py --config /app/config.txt >> /proc/1/fd/1 2>&1" > /etc/cron.d/rtve-job
+    echo "$CRON_EXPRESSION root python3 /app/script.py --config /app/config.txt >> /proc/1/fd/1 2>&1" > /etc/cron.d/rtve-job
     chmod 0644 /etc/cron.d/rtve-job
-    crontab /etc/cron.d/rtve-job
     echo "Starting cron daemon in foreground..."
     cron -f
 else
     echo "No CRON_EXPRESSION set, running script once..."
-    exec python /app/script.py "$@"
+    exec python3 /app/script.py "$@"
 fi
