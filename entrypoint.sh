9c6127cb70085ebee52b20973481c52ba8d80d3c
suken27 <suken2727@gmail.com>
2026-04-29 01:59:24 +0200

    fix call of the python script on the entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,12 @@ if [ -n "$CRON_EXPRESSION" ]; then
     cat > /etc/cron.d/rtve-job <<'EOF'
 SHELL=/bin/sh
 PATH=/usr/local/bin:/usr/bin:/bin
-$CRON_EXPRESSION root /usr/local/bin/python3 /app/script.py --config /app/config.txt >> /proc/1/fd/1 2>&1 --dest /downloads
+$CRON_EXPRESSION root /usr/local/bin/python3 /app/script.py --dest /downloads --config /app/config.txt >> /proc/1/fd/1 2>&1
 EOF
     chmod 0644 /etc/cron.d/rtve-job
     echo "Starting cron daemon in foreground..."
     cron -f
 else
     echo "No CRON_EXPRESSION set, running script once..."
-    exec /usr/local/bin/python3 /app/script.py "$@"
+    exec /usr/local/bin/python3 /app/script.py --dest /downloads --config /app/config.txt "$@"
 fi
